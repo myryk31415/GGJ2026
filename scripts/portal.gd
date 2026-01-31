ccae163125d5202342df2920e8b70cfe7f70b92f
Paul Adam <adamp@posteo.de>
2026-01-31 08:00:34 +0100

    Moving scripts to script folder and adding basic Portal scene

---
+++ scripts/portal.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var player: Array[Node2D] = %Area2D.get_overlapping_bodies()
+	var teleport_progress = 0.0
+	if player.size() > 0:
+		var distance: float = global_position.x - player[0].global_position.x
+		if distance < 0:
+			teleport_progress = 100
+		else:
+			teleport_progress = 1 - distance / %PlayerDetection.shape.size.x
+	print(teleport_progress)
