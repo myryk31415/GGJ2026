100e13f46bc0388aa5505836a21d7a98ffc7f08d
Paul Adam <adamp@posteo.de>
2026-02-18 22:29:40 +0100

    Add a tutorial :D

--- scenes/tutorial.gd
+++ scenes/tutorial.gd
@@ -12,11 +12,5 @@ func _process(_delta: float) -> void:
 
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
-func _on_how_to_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
